7994f99fed5df75dd093d5d4c1921c1e2231dd75
fiandev <wongkeras2@gmail.com>
2023-04-12 06:41:26 +0700

    Change: move controllers to specific Author Folder

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AnimeApiController;
+use App\Http\Controllers\API\Fdev\AnimeApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
