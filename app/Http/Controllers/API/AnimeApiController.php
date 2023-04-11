da2e37caeeddd39bc098ecebf22c51e7a7a33a01
fiandev <wongkeras2@gmail.com>
2023-04-11 21:27:37 +0700

    Add: API/AnimeApiController & Model Anime

--- app/Http/Controllers/API/AnimeApiController.php
+++ app/Http/Controllers/API/AnimeApiController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Anime;
 use Illuminate\Http\Request;
+use Weidner\Goutte;
 
 class AnimeApiController extends Controller
 {
+    const URL = "https://otakudesu.lol";
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $crawler = Goutte::request("GET", self::URL);
+        $result = [];
+        $res = $crawler->filter("#abtext > .bariskelom");
+        
+        dd($res);
+        return json_encode($crawler);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//
+Route::controller(\App\Http\Controllers\API\AnimeApiController::class)->group(function(){
+  Route::get("/anime/all", "index");
+});
